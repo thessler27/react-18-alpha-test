b08d4991f22aeea1c76e105bcffb3ae2dc8faccb
Thomas Hessler <thomas@meanguppy.com>
2021-11-10 14:27:48 -0500

    remove server-build

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Thomas's Awesome Site" />
           <link rel="shortcut icon" href="favicon.ico" />
           <title>{title}</title>
         </head>
